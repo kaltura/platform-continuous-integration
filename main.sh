050a90ece50cbe3dee7493d559c781f7ee4e32fb
jessp01 <jess.portnoy@kaltura.com>
2014-03-07 13:27:05 -0500

    sometimes, this takes longer so.. lets increase search range.

--- main.sh
+++ main.sh
@@ -27,8 +27,8 @@ install_kalt_allin1 $NIGHTLY_RELEASE_RPM_URL $KALTURA_NODE_IMG 1;
 MAIL_TO="jess.portnoy@kaltura.com"
 #MAIL_TO="jess.portnoy@kaltura.com,zohar.babin@kaltura.com"
 EPOCH_CURR=`date +%s`
-MIN_TIMESTAMP=`expr $EPOCH_CURR - 3600`
-MAX_TIMESTAMP=`expr $EPOCH_CURR + 3600`
+MIN_TIMESTAMP=`expr $EPOCH_CURR - 7200`
+MAX_TIMESTAMP=`expr $EPOCH_CURR + 7200`
 CSV_FILE=`create_csv $MIN_TIMESTAMP $MAX_TIMESTAMP`
 echo "Sanity for Kaltura $MY_BASE_VERSION" | mutt -s "Sanity for Kaltura $BASE_VERSION" $MAIL_TO -a $CSV_FILE
 
