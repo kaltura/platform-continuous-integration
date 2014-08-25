810e0516aac5b32ab36415195eaa6b65559ac803
root <root@ce-csi.dev.kaltura.com>
2014-08-25 11:41:20 -0400

    have not committed this in a while

--- run_sanity.sh
+++ run_sanity.sh
@@ -7,10 +7,17 @@ for i in `cat $CLUS_LIST`;do
 	if echo $i|grep '#' -q ;then
 		continue
 	fi
+	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "yum clean all && yum update '*kaltura*' -y"
 	if [ -z "$VERSION" ];then
 		VERSION=`ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "rpm -qa kaltura-base --queryformat %{version}" `
 	fi
-	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "yum clean all && yum update '*kaltura*' -y"
+	if [ -z "$DB_UPDATE" ];then
+
+		ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "/opt/kaltura/bin/kaltura-db-update.sh"
+		DB_UPDATE=1
+
+	fi
+	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "/opt/kaltura/bin/kaltura-base-config.sh /root/kalt.ans && /opt/kaltura/bin/kaltura-batch-config.sh"
 	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /opt/kaltura/bin/kaltura-sanity.sh 
 	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$i:"/tmp/$i-reportme.`date +%d_%m_%Y`.sql" /tmp/
 	cat "/tmp/$i-reportme.`date +%d_%m_%Y`.sql" >> /tmp/reportme.`date +%d_%m_%Y`.sql
@@ -25,4 +32,4 @@ FAILED=0
 SUCCESS=`echo "select count(rc) from csi_log where rc=0 and kaltura_version='$VERSION';"|sqlite3 $DB`
 FAILED=`echo "select count(rc) from csi_log where rc!=0 and kaltura_version='$VERSION';"|sqlite3 $DB`
 echo "insert into success_rates values(NULL,$FAILED,$SUCCESS,'$VERSION');"|sqlite3 $DB
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /opt/vhosts/csi/db/csi.db root@54.196.169.30:/opt/vhosts/csi/db/ 
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /opt/vhosts/csi/db/csi.db root@38.74.192.73:/opt/vhosts/reports/ci/db 
