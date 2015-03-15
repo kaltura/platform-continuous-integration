5f64b25a9207893d8f245ab32758d10b7fef93cc
CSI <community@kaltura.com>
2015-03-15 09:47:05 -0400

    ENV vars instead of repeating literal paths.

--- run_sanity.sh
+++ run_sanity.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-CLUS_LIST="/opt/vhosts/csi/cluster_members"
-
-DB="/opt/vhosts/csi/db/csi.db"
-ALERTS_ADDR="community@kaltura.com"
+CSI_BASE_DIR="/opt/vhosts/csi"
+CLUS_LIST="$CSI_BASE_DIR/cluster_members"
+KALTURA_BASE="/opt/kaltura"
+CSI_WEB_IF_HOST=38.74.192.73
+DB="$CSI_BASE_DIR/db/csi.db"
+ALERTS_ADDR="jess.portnoy@kaltura.com"
 rm /tmp/reportme.`date +%d_%m_%Y`.sql
 for i in `cat $CLUS_LIST`;do
 	if echo $i|grep '#' -q ;then
@@ -14,12 +16,15 @@ for i in `cat $CLUS_LIST`;do
 	fi
 	if [ -z "$DB_UPDATE" ];then
 
-		ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "/opt/kaltura/bin/kaltura-db-update.sh"
+		ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "$KALTURA_BASE/bin/kaltura-db-update.sh"
 		DB_UPDATE=1
 
 	fi
-	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "/opt/kaltura/bin/kaltura-base-config.sh /root/kalt.ans && /opt/kaltura/bin/kaltura-batch-config.sh"
-	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /opt/kaltura/bin/kaltura-sanity.sh 
+	if ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no rpm -q kaltura-sphinx;then
+		ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $KALTURA_BASE/bin/kaltura-sphinx-config.sh /root/kalt.ans
+	fi
+	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "$KALTURA_BASE/bin/kaltura-base-config.sh /root/kalt.ans && $KALTURA_BASE/bin/kaltura-batch-config.sh"
+	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $KALTURA_BASE/bin/kaltura-sanity.sh 
 	RC=$?
 	if [ $RC -ne 0 ];then 
 		MSG="Sanity on machine $i aborted."
@@ -41,4 +46,5 @@ FAILED=0
 SUCCESS=`echo "select count(rc) from csi_log where rc=0 and kaltura_version='$VERSION';"|sqlite3 $DB`
 FAILED=`echo "select count(rc) from csi_log where rc!=0 and kaltura_version='$VERSION';"|sqlite3 $DB`
 echo "insert into success_rates values(NULL,$FAILED,$SUCCESS,'$VERSION');"|sqlite3 $DB
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /opt/vhosts/csi/db/csi.db root@38.74.192.73:/opt/vhosts/reports/ci/db 
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $CSI_BASE_DIR/db/csi.db root@$CSI_WEB_IF_HOST:/opt/vhosts/reports/ci/db 
+ssh root@ce-front0.dev.kaltura.com -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /usr/local/bin/clientlibs_test.sh
