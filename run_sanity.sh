1d78ac1a4d90e7d631b06dcd19082bce7280fc92
CSI <community@kaltura.com>
2015-07-17 08:58:12 -0400

    we now support running on deb machines as well.

--- run_sanity.sh
+++ run_sanity.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 CSI_BASE_DIR="/opt/vhosts/csi"
-CLUS_LIST="$CSI_BASE_DIR/cluster_members"
+RPM_CLUS_LIST="$CSI_BASE_DIR/rpm_cluster_members"
+DEB_CLUS_LIST="$CSI_BASE_DIR/deb_cluster_members"
 KALTURA_BASE="/opt/kaltura"
-CSI_WEB_IF_HOST=38.74.192.73
+CSI_WEB_IF_HOST=216.200.17.80
 DB="$CSI_BASE_DIR/db/csi.db"
 ALERTS_ADDR="jess.portnoy@kaltura.com"
 rm /tmp/reportme.`date +%d_%m_%Y`.sql
-for i in `cat $CLUS_LIST`;do
+for i in `cat $RPM_CLUS_LIST`;do
 	if echo $i|grep '#' -q ;then
 		continue
 	fi
@@ -36,6 +37,35 @@ for i in `cat $CLUS_LIST`;do
 	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$i:"/tmp/$i-reportme.`date +%d_%m_%Y`.sql" /tmp/
 	cat "/tmp/$i-reportme.`date +%d_%m_%Y`.sql" >> /tmp/reportme.`date +%d_%m_%Y`.sql
 done
+
+for i in `cat $DEB_CLUS_LIST`;do
+	if echo $i|grep '#' -q ;then
+		continue
+	fi
+	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "aptitude update && /root/kalt.ans && aptitude -y dist-upgrade \"~Nkaltura\""
+	if [ -z "$VERSION" ];then
+		VERSION=`ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "dpkg-query -W -f'${Version}' kaltura-base|awk -F "-" '{print $1}'" `
+	fi
+	if ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no dpkg -l kaltura-batch;then
+		ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "/root/kalt.ans && export DEBIAN_FRONTEND=noninteractive; dpkg-reconfigure kaltura-base && dpkg-reconfigure kaltura-batch" 
+	fi
+	if ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no dpkg -l kaltura-front;then
+		ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "/root/kalt.ans && dpkg-reconfigure kaltura-base && dpkg-reconfigure kaltura-front" 
+	fi
+	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $KALTURA_BASE/bin/kaltura-sanity.sh 
+	RC=$?
+	if [ $RC -ne 0 ];then 
+		MSG="Sanity on machine $i aborted."
+		if [ $RC -eq 11 ];then
+			MSG="${MSG} Machine out of space. Do something"
+		fi
+		echo $MSG| mail -s "Sanity for machine $i aborted!" $ALERTS_ADDR
+	fi
+	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$i:"/tmp/$i-reportme.`date +%d_%m_%Y`.sql" /tmp/
+	cat "/tmp/$i-reportme.`date +%d_%m_%Y`.sql" >> /tmp/reportme.`date +%d_%m_%Y`.sql
+done
+#ssh root@ce-ubuntu0.dev.kaltura.com "/root/kalt.ans && /root/install_kaltura_all_in_1.sh && /opt/kaltura/bin/kaltura-sanity.sh"
+
 
 cp $DB $DB.older
 echo "delete from csi_log where kaltura_version='$VERSION';"|sqlite3 $DB
