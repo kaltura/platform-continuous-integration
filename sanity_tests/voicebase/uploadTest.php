c37103b43dff5e49c9ae7bce9d01fd5c5cb42d1f
Jess Portnoy <jess.portnoy@kaltura.com>
2015-09-08 13:28:17 +0100

    .

--- sanity_tests/voicebase/uploadTest.php
+++ sanity_tests/voicebase/uploadTest.php
@@ -1,13 +1,32 @@
 <?php
-if ($argc < 2 ){
-    echo 'Usage: ' . $argv[0]." <url/to/entry>\n";
+if ($argc < 4){
+    echo 'Usage: ' . $argv[0]." <service URL> <partner ID> <admin secret> <entry ID>\n";
     exit (1);
 }
-$entry_url=$argv[1];
+$service_url=$argv[1];
+$partner_id=$argv[2];
+$admin_secret=$argv[3];
+$entry_id=$argv[4];
+require_once('/opt/kaltura/web/content/clientlibs/php5/KalturaClient.php');
+$config = new KalturaConfiguration($partner_id);
+$config->serviceUrl = $service_url;
+$client = new KalturaClient($config);
+$expiry = null;
+$privileges = null;
+$userId = null;
+$type = KalturaSessionType::ADMIN;
+$ks = $client->session->start($admin_secret, $userId, $type, $partner_id, $expiry, $privileges);
+$client->setKs($ks);
+$filter = new KalturaAssetFilter();
+$filter->entryIdEqual = $entry_id;
+$filter->tagsLike = 'source';
+$pager = null;
+$result = $client->flavorAsset->listAction($filter, $pager);
+$mediaUrl = $client->flavorAsset->geturl($result->objects[0]->id, null, null, null); 
 require_once('voicebase_account.inc');
-$url = VOICEBASE_ENDPOINT.'?version='.API_VERSION.'&apiKey='.API_KEY.'&password='.API_PASSWD.'&action=uploadMedia&externalID=';
+$url = VOICEBASE_ENDPOINT.'?version='.API_VERSION.'&apiKey='.API_KEY.'&password='.API_PASSWD.'&action=uploadMedia&externalID='.$entry_id;
 
-$postfields = array("mediaUrl" => $entry_url );
+$postfields = array("mediaUrl" => $mediaUrl );
 $ch = curl_init();
 $options = array(
     CURLOPT_URL => $url,
@@ -16,7 +35,7 @@ $options = array(
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_SSL_VERIFYHOST => 0,
     CURLOPT_SSL_VERIFYPEER =>0,
-); // cURL options
+); 
 curl_setopt_array($ch, $options);
 $result = json_decode(curl_exec($ch),true);
 var_dump($result);
--- sanity_tests/voicebase/uploadTranscript.php
+++ sanity_tests/voicebase/uploadTranscript.php
@@ -1,14 +1,13 @@
 <?php
-if ($argc < 6){
-    echo 'Usage: ' . $argv[0]." <service URL> <partner ID> <admin secret> <url/to/entry> </path/to/transcript/file> <job ID>\n";
+if ($argc < 5){
+    echo 'Usage: ' . $argv[0]." <service URL> <partner ID> <admin secret> <entry ID> </path/to/transcript/file>\n";
     exit (1);
 }
 $service_url=$argv[1];
 $partner_id=$argv[2];
 $admin_secret=$argv[3];
-$entry_url=$argv[4];
+$entry_id=$argv[4];
 $transcript_file=$argv[5];
-$job_id=$argv[6];
 require_once('/opt/kaltura/web/content/clientlibs/php5/KalturaClient.php');
 $config = new KalturaConfiguration($partner_id);
 $config->serviceUrl = $service_url;
@@ -18,13 +17,20 @@ $privileges = null;
 $userId = null;
 $type = KalturaSessionType::ADMIN;
 $ks = $client->session->start($admin_secret, $userId, $type, $partner_id, $expiry, $privileges);
+$client->setKs($ks);
+$filter = new KalturaAssetFilter();
+$filter->entryIdEqual = $entry_id;
+$filter->tagsLike = 'source';
+$pager = null;
+$result = $client->flavorAsset->listAction($filter, $pager);
+$mediaUrl = $client->flavorAsset->geturl($result->objects[0]->id, null, null, null); 
 
 require_once('voicebase_account.inc');
-$callback_url=urlencode("$service_url/api_v3/index.php/service/integration_integration/action/notify/id/$job_id/ks/$ks");
-$url = VOICEBASE_ENDPOINT.'?version='.API_VERSION.'&apiKey='.API_KEY.'&password='.API_PASSWD.'&action=uploadMedia&externalID=&machineReadyCallBack='.$callback_url;
+//$callback_url=urlencode("$service_url/api_v3/index.php/service/integration_integration/action/notify/id/$job_id/ks/$ks");
+$url = VOICEBASE_ENDPOINT.'?version='.API_VERSION.'&apiKey='.API_KEY.'&password='.API_PASSWD.'&action=uploadMedia&externalID='.$entry_id ;//&machineReadyCallBack='.$callback_url;
 $ch = curl_init();
-$postfields = array("mediaUrl" => $entry_url,
-    "transcript" => $transcript_file,
+$postfields = array("mediaUrl" => $mediaUrl,
+    "transcript" => '@'.$transcript_file,
     'transcriptType' => 'human');
 $options = array(
     CURLOPT_URL => $url,
