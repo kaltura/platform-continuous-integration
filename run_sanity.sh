08daf7e6dbf737e861313f2c1e5ac60c15ea0b90
CSI <community@kaltura.com>
2017-10-31 07:11:01 -0400

    main.sh is no longer used or needed, run_sanity.sh is the main script proper placeholders for sanity_tests/utils.rc

--- run_sanity.sh
+++ run_sanity.sh
@@ -3,7 +3,8 @@ CSI_BASE_DIR="/opt/vhosts/csi"
 RPM_CLUS_LIST="$CSI_BASE_DIR/rpm_cluster_members"
 DEB_CLUS_LIST="$CSI_BASE_DIR/deb_cluster_members"
 KALTURA_BASE="/opt/kaltura"
-CSI_WEB_IF_HOST=216.200.17.80
+CSI_WEB_IF_HOST=
+CSI_CLIENT_GENERATING_HOST=
 DB="$CSI_BASE_DIR/db/csi.db"
 ALERTS_ADDR="jess.portnoy@kaltura.com"
 rm /tmp/reportme.`date +%d_%m_%Y`.sql
@@ -11,7 +12,7 @@ for i in `cat $RPM_CLUS_LIST`;do
 	if echo $i|grep '#' -q ;then
 		continue
 	fi
-	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "yum clean all && yum update '*kaltura*' -y"
+	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "yum clean all ;yum update '*kaltura*' -y ;/etc/init.d/kaltura-batch stop||true"
 	if [ -z "$VERSION" ];then
 		VERSION=`ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "rpm -qa kaltura-base --queryformat %{version}" `
 	fi
@@ -24,7 +25,7 @@ for i in `cat $RPM_CLUS_LIST`;do
 	if ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no rpm -q kaltura-sphinx;then
 		ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $KALTURA_BASE/bin/kaltura-sphinx-config.sh /root/kalt.ans
 	fi
-	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "$KALTURA_BASE/bin/kaltura-base-config.sh /root/kalt.ans && $KALTURA_BASE/bin/kaltura-batch-config.sh"
+	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "$KALTURA_BASE/bin/kaltura-base-config.sh /root/kalt.ans && $KALTURA_BASE/bin/kaltura-batch-config.sh && /etc/init.d/kaltura-batch stop"
 	ssh root@$i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $KALTURA_BASE/bin/kaltura-sanity.sh 
 	RC=$?
 	if [ $RC -ne 0 ];then 
@@ -37,7 +38,6 @@ for i in `cat $RPM_CLUS_LIST`;do
 	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$i:"/tmp/$i-reportme.`date +%d_%m_%Y`.sql" /tmp/
 	cat "/tmp/$i-reportme.`date +%d_%m_%Y`.sql" >> /tmp/reportme.`date +%d_%m_%Y`.sql
 done
-
 for i in `cat $DEB_CLUS_LIST`;do
 	if echo $i|grep '#' -q ;then
 		continue
@@ -64,8 +64,10 @@ for i in `cat $DEB_CLUS_LIST`;do
 	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$i:"/tmp/$i-reportme.`date +%d_%m_%Y`.sql" /tmp/
 	cat "/tmp/$i-reportme.`date +%d_%m_%Y`.sql" >> /tmp/reportme.`date +%d_%m_%Y`.sql
 done
-#ssh root@ce-ubuntu0.dev.kaltura.com "/root/kalt.ans && /root/install_kaltura_all_in_1.sh && /opt/kaltura/bin/kaltura-sanity.sh"
 
+
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$SINGLE_DEB_MACHINE:"/tmp/$SINGLE_DEB_MACHINE-reportme.`date +%d_%m_%Y`.sql" /tmp/
+cat "/tmp/$SINGLE_DEB_MACHINE-reportme.`date +%d_%m_%Y`.sql" >> /tmp/reportme.`date +%d_%m_%Y`.sql
 
 cp $DB $DB.older
 echo "delete from csi_log where kaltura_version='$VERSION';"|sqlite3 $DB
@@ -77,4 +79,7 @@ SUCCESS=`echo "select count(rc) from csi_log where rc=0 and kaltura_version='$VE
 FAILED=`echo "select count(rc) from csi_log where rc!=0 and kaltura_version='$VERSION';"|sqlite3 $DB`
 echo "insert into success_rates values(NULL,$FAILED,$SUCCESS,'$VERSION');"|sqlite3 $DB
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $CSI_BASE_DIR/db/csi.db root@$CSI_WEB_IF_HOST:/opt/vhosts/reports/ci/db 
-ssh root@ce-front0.dev.kaltura.com -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /usr/local/bin/clientlibs_test.sh
+MSG=`ssh root@$CSI_CLIENT_GENERATING_HOST -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /usr/local/bin/clientlibs_test.sh`
+if [ $? -ne 0 ];then
+		echo $MSG| mail -s "Client lib gen failed" $ALERTS_ADDR
+fi
